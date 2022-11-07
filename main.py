9fa2944dbea3c8b6158d3ae1da2819df59838207
yeongsang2 <dudtkd1221@gmail.com>
2022-11-08 00:46:49 +0900

    색상, 브랜드 로고, 무늬 모델 1차 합치기

--- main.py
+++ main.py
@@ -7,26 +7,31 @@ from color_classification import color
 sys.path.append('C:\\ai_closet_demo')
 from yolov5 import detect
 
+sys.path.append('C:\\ai_closet_demo\\pattern_recognize')
+from pattern_recognize import pattern
+
 if __name__ == "__main__":
+    
+    src = "C:\\ai_closet_demo\\image\\nike.jpg"
     
     result = ""
     # 색상분류
     que = Queue()
-    que = color.execute("C:\\ai_closet_demo\image\\adidas.png", que)
+    que = color.execute(src, que)
     # print(que.get())
     result += f'{que.get()}'
 
     # 브랜드 로고 식별
-    detect.main_ide("C:\\ai_closet_demo\image\\adidas.png")
+    detect.main_ide(src)
     if(detect.logo_name):
-        result += f'{detect.logo_name} 입니다. '
+        result += f'{detect.logo_name}, '
     # print(detect.logo_name)
 
-    # 무늬 식별
-    
+    # 무늬 식별 
+    pattern.classify_pattern(src)
+    result += f'{pattern.pattern_name}'
 
-
-    print(result)
+    print(result + "입니다.")
 
 
 
--- pattern_recognize/pattern.py
+++ pattern_recognize/pattern.py
@@ -9,8 +9,20 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
+global pattern_name
+
+
+transforms_test = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    # transforms.CenterCrop(120)
+    ])
 
 def imshow(input, title):
+
+
+
     # torch.Tensor를 numpy 객체로 변환
     input = input.numpy().transpose((1, 2, 0))
     # 이미지 정규화 해제하기
@@ -23,27 +35,26 @@ def imshow(input, title):
     plt.title(title)
     plt.show()
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = models.resnet34()
-num_ftrs = model.fc.in_features
-model.fc = nn.Linear(num_ftrs, 3)  # make the change
-model.load_state_dict(torch.load('C:\\ai_closet_demo\\pattern_recognize\\model_weights.pth', map_location=device))
-model.to(device)
+def classify_pattern(src):
+   
+    global pattern_name
+    ## 학습모델 연결
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = models.resnet34()
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, 3)  # make the change
+    model.load_state_dict(torch.load('C:\\ai_closet_demo\\pattern_recognize\\model_weights.pth', map_location=device))
+    model.to(device)
 
-transforms_test = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-    # transforms.CenterCrop(120)
-])
+    image = Image.open(src).convert('RGB')
+    image = transforms_test(image).unsqueeze(0).to(device)
 
+    class_names = ['checked pattern', 'flower pattern', 'stripe pattern']
 
-image = Image.open('C:\\ai_closet_demo\\image\\adidas.PNG')
-image = transforms_test(image).unsqueeze(0).to(device)
+    with torch.no_grad():
+        outputs = model(image) # 모델에 이미지 넣기
+        _, preds = torch.max(outputs, 1)
+        pattern_name = class_names[preds[0]]
+        # imshow(image.cpu().data[0], title='Predict : ' + class_names[preds[0]]) ## 생략
 
-class_names = ['checked pattern', 'flower pattern', 'stripe pattern']
-
-with torch.no_grad():
-    outputs = model(image) # 모델에 이미지 넣기
-    _, preds = torch.max(outputs, 1)
-    imshow(image.cpu().data[0], title='Predict : ' + class_names[preds[0]])
+    
