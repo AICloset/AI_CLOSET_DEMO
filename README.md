8f224019b86e0356675c614e6a6164f0bf092ebc
yeongsang2 <dudtkd1221@gmail.com>
2022-11-08 11:11:03 +0900

    fix readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# AI_CLOSET_DEMO
+### AI_CLOSET_DEMO
+* 중간발표를 위한 DEMO
+* image input -> 옷 색상, 브랜드 로고, 무늬 출력
--- main.py
+++ main.py
@@ -12,26 +12,26 @@ from pattern_recognize import pattern
 
 if __name__ == "__main__":
     
-    src = "C:\\ai_closet_demo\\image\\nike.jpg"
+    src = "C:\\ai_closet_demo\\image\\nike2.jpg"
     
+    print("색분류")
     result = ""
     # 색상분류
     que = Queue()
     que = color.execute(src, que)
     # print(que.get())
     result += f'{que.get()}'
+    print("로고----------")
 
     # 브랜드 로고 식별
     detect.main_ide(src)
     if(detect.logo_name):
         result += f'{detect.logo_name}, '
     # print(detect.logo_name)
+    print("-무늬---------")
 
     # 무늬 식별 
     pattern.classify_pattern(src)
     result += f'{pattern.pattern_name}'
 
     print(result + "입니다.")
-
-
-
