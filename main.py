03b5bf2e2d15c162f1d7a819708504cb692d0cd7
yeongsang2 <dudtkd1221@gmail.com>
2022-11-10 00:58:58 +0900

    이미지 경로 프롬포트에서 매개변수 전달

--- main.py
+++ main.py
@@ -12,23 +12,25 @@ from pattern_recognize import pattern
 
 if __name__ == "__main__":
     
-    src = "C:\\ai_closet_demo\\image\\nike2.jpg"
+    src = sys.argv[1]
+
+    # src = "C:\\ai_closet_demo\\image\\polo.jpeg"
     
-    print("색분류")
+    # print("색분류")
     result = ""
     # 색상분류
     que = Queue()
     que = color.execute(src, que)
     # print(que.get())
     result += f'{que.get()}'
-    print("로고----------")
+    # print("로고----------")
 
     # 브랜드 로고 식별
     detect.main_ide(src)
     if(detect.logo_name):
         result += f'{detect.logo_name}, '
     # print(detect.logo_name)
-    print("-무늬---------")
+    # print("-무늬---------")
 
     # 무늬 식별 
     pattern.classify_pattern(src)
